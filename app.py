9cf65d65e4dc5486e31c9dea90b940a1acd8b7bb
Milo Bernfield-Millman <milorobin@gmail.com>
2016-05-17 10:00:04 -0400

    started utils.py

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/login", methods = ["POST"])
+def login():
+    ## Login template will have a link to register
+    if str(request.form["button"]) == "Log in":
+        username = str(request.form["username"])
+        ## User logs in by checking off account type (student or advisor), inputing uname and pword
+        if utils.pwordAuth(username, str(request.form["password"]), request.form["accounttype"]):
+            return render_template("/home.html")
+        else:
+            return render_template("/login.html", text = "Username and password do not match")
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,3 @@
+## checks if the username/password/accounttype combination is correct
+def pwordAuth(uname, pword, atype):
+    return True
