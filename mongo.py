d2ac729ac3f176f2fe3560742dd860eb9d9fa73c
lin <ivlin112@gmail.com>
2016-05-21 13:04:44 -0400

    mongo code

---
+++ mongo.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient
+
+#authentication
+def authenticate():
+    pass
+
+#adds an entry to the specified database and collection
+def addEntry(database,collection,entry):
+    client = MongoClient()
+    db = client[database]
+    collection = db[collection]
+    collection.insert(entry)
+    client.close()
+
+#lists all entries
+def listEntry(database,collection):
+    client = MongoClient()
+    collection = client[database][collection]
+    for entry in collection.find():
+        print entry
+    client.close()
+
+#retrieves the specified entry
+def getEntry(database,collection,query):
+    client = MongoClient()
+    collection = client[database][collection]
+    entry = collection.find_one(query)
+    client.close()
+    return entry
+
+#finds the entry in the specified database's collection
+#matching the specifications in the query
+#updates the fields from revisedEntry
+def editEntry(database,collection,query,revisedEntry):
+    client = MongoClient()
+    collection = client[database][collection]
+    entry = collction.find_one(query)
+    for property in revisedEntry:
+        print property
+        entry[property] = revisedEntry[property]
+    client.close()
