d45c06ce2331886994c941c0e67045932d0c7dcb
lin <ivlin112@gmail.com>
2016-05-22 21:25:29 -0400

    emails

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from passlib.hash import sha512_crypt
 import mongo
+import smtplib
 
 ##creates new user account
 def createUser(uname, pword, atype):
@@ -13,3 +14,18 @@ def createUser(uname, pword, atype):
 def pwordAuth(uname, pword, atype):
     user = mongo.getEntry("modelun","users",{"username":uname,"type":atype})
     return user is not None and sha512_crypt.verify(pword,user["password"])
+
+##email blasts
+##requires gmail account, as well as the user to allow less secure apps
+##on the account settings
+def emailUser(username, password, receiver, subject, message):
+    smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo()
+    smtpserver.login(username, password)
+    message = "To: {}\nFrom: {}\nSubject: {}\n".format(receiver,username,subject)
+    message += '\n this is test msg  \n\n'
+    smtpserver.sendmail(username, receiver, message)
+    smtpserver.close()
+
