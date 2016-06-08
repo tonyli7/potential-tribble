786335422e323bf517cb1321c9931b654dbd69a2
lin <ivlin112@gmail.com>
2016-06-08 11:57:27 -0400

    del w conflict

--- static/js/home.js
+++ static/js/home.js
@@ -1,15 +1,14 @@
 console.log("HI");
 function countdown(yr,m,d,h,min,sec){
-    theyear=yr;themonth=m;theday=d
     var today=new Date()
-    var todayy=y-today.getYear()
+    var todayy=yr-today.getYear()
     var todaym=m-today.getMonth()
     var todayd=d-today.getDate()
     var todayh=h-today.getHours()
     var todaymin=min-today.getMinutes()
     var todaysec=sec-today.getSeconds()
 
-    console.log(todayy+""+todaym+""+todayd)
+    console.log(todayy+"years "+todaym+"months"+todayd+"seconds")
     setTimeout(countdown(yr,m,d,h,min,sec),1000)
 }
 
